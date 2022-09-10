e1569281f7efac7c6125ef54f221e33021eab253
Noel <cutie@floofy.dev>
2022-09-10 09:08:56 -0700

    Lint with Spotless, fix pre-commit hook again :sparkles:

--- lib/elasticsearch/src/test/kotlin/ElasticsearchTests.kt
+++ lib/elasticsearch/src/test/kotlin/ElasticsearchTests.kt
@@ -17,31 +17,31 @@
 
 package org.noelware.charted.search.elasticsearch.tests
 
-//import kotlinx.coroutines.runBlocking
-//import kotlinx.serialization.json.Json
-//import okhttp3.internal.closeQuietly
-//import org.junit.Test
-//import org.junit.jupiter.api.Disabled
-//import org.junit.jupiter.api.assertThrows
-//import org.junit.jupiter.api.condition.DisabledOnOs
-//import org.junit.jupiter.api.condition.OS
-//import org.noelware.charted.configuration.dsl.Config
-//import org.noelware.charted.configuration.dsl.search.ElasticsearchConfig
-//import org.noelware.charted.configuration.dsl.search.SearchConfig
-//import org.noelware.charted.elasticsearch.DefaultElasticsearchService
-//import org.noelware.charted.elasticsearch.ElasticsearchService
-//import org.slf4j.LoggerFactory
-//import org.testcontainers.containers.output.Slf4jLogConsumer
-//import org.testcontainers.elasticsearch.ElasticsearchContainer
-//import org.testcontainers.junit.jupiter.Testcontainers
-//import org.testcontainers.utility.DockerImageName
-//import kotlin.test.assertEquals
-//import kotlin.test.assertFalse
+// import kotlinx.coroutines.runBlocking
+// import kotlinx.serialization.json.Json
+// import okhttp3.internal.closeQuietly
+// import org.junit.Test
+// import org.junit.jupiter.api.Disabled
+// import org.junit.jupiter.api.assertThrows
+// import org.junit.jupiter.api.condition.DisabledOnOs
+// import org.junit.jupiter.api.condition.OS
+// import org.noelware.charted.configuration.dsl.Config
+// import org.noelware.charted.configuration.dsl.search.ElasticsearchConfig
+// import org.noelware.charted.configuration.dsl.search.SearchConfig
+// import org.noelware.charted.elasticsearch.DefaultElasticsearchService
+// import org.noelware.charted.elasticsearch.ElasticsearchService
+// import org.slf4j.LoggerFactory
+// import org.testcontainers.containers.output.Slf4jLogConsumer
+// import org.testcontainers.elasticsearch.ElasticsearchContainer
+// import org.testcontainers.junit.jupiter.Testcontainers
+// import org.testcontainers.utility.DockerImageName
+// import kotlin.test.assertEquals
+// import kotlin.test.assertFalse
 //
-//@DisabledOnOs(value = [OS.MAC, OS.WINDOWS])
-//@Testcontainers(disabledWithoutDocker = true)
-//@Disabled("Can't run the Docker image (for now), will need to fix.")
-//class ElasticsearchTests {
+// @DisabledOnOs(value = [OS.MAC, OS.WINDOWS])
+// @Testcontainers(disabledWithoutDocker = true)
+// @Disabled("Can't run the Docker image (for now), will need to fix.")
+// class ElasticsearchTests {
 //    private val container: ElasticsearchContainer = ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch").withTag("8.4.1"))
 //        .apply {
 //            withLogConsumer(Slf4jLogConsumer(LoggerFactory.getLogger("co.elastic.elasticsearch.docker")))
@@ -85,4 +85,4 @@ package org.noelware.charted.search.elasticsearch.tests
 //            assertEquals("8.4.1", this.serverVersion)
 //        }
 //    }
-//}
+// }
