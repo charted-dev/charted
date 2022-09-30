b64fffd12854661c4a000a06bdaf7734c99ef695
Noel <cutie@floofy.dev>
2022-09-30 05:30:16 -0700

    :sparkles: Spotless

--- testing/kubernetes/src/test/java/org/noelware/charted/testing/kubernetes/k3s/K3sTests.java
+++ testing/kubernetes/src/test/java/org/noelware/charted/testing/kubernetes/k3s/K3sTests.java
@@ -31,7 +31,9 @@ import org.junit.jupiter.api.condition.OS;
 import org.noelware.charted.testing.kubernetes.KubernetesEnvironment;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@DisabledOnOs(value = {OS.WINDOWS, OS.MAC}, architectures = {"arm64", "aarch64"})
+@DisabledOnOs(
+        value = {OS.WINDOWS, OS.MAC},
+        architectures = {"arm64", "aarch64"})
 @Testcontainers(disabledWithoutDocker = true)
 public class K3sTests {
     private static final K3SContainer container = new K3SContainer();
