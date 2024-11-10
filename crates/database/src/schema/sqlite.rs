57cdfce9193b032dde107beeb87e46756b7691aa
Noel <cutie@floofy.dev>
2024-11-09 20:11:05 -0800

    Fix overflow error in `PUT /apikeys`

--- crates/database/src/schema/sqlite.rs
+++ crates/database/src/schema/sqlite.rs
@@ -1,17 +1,4 @@
-// 🐻‍❄️📦 charted-server: Free, open source, and reliable Helm Chart registry made in Rust
-// Copyright 2022-2024 Noelware, LLC. <team@noelware.org>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// @generated automatically by Diesel CLI.
 
 diesel::table! {
     use diesel::sql_types::*;
@@ -21,7 +8,7 @@ diesel::table! {
         description -> Nullable<Text>,
         created_at -> TimestamptzSqlite,
         updated_at -> TimestamptzSqlite,
-        expires_in -> Nullable<Timestamp>,
+        expires_in -> Nullable<TimestamptzSqlite>,
         scopes -> BigInt,
         owner -> Text,
         token -> Text,
@@ -163,15 +150,15 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(api_keys -> users(owner));
-diesel::joinable!(organization_members -> organizations(organization));
-diesel::joinable!(organization_members -> users(account));
-diesel::joinable!(organizations -> users(owner));
-diesel::joinable!(repository_members -> repositories(repository));
-diesel::joinable!(repository_members -> users(account));
-diesel::joinable!(repository_releases -> repositories(repository));
-diesel::joinable!(sessions -> users(owner));
-diesel::joinable!(user_connections -> users(account));
+diesel::joinable!(api_keys -> users (owner));
+diesel::joinable!(organization_members -> organizations (organization));
+diesel::joinable!(organization_members -> users (account));
+diesel::joinable!(organizations -> users (owner));
+diesel::joinable!(repository_members -> repositories (repository));
+diesel::joinable!(repository_members -> users (account));
+diesel::joinable!(repository_releases -> repositories (repository));
+diesel::joinable!(sessions -> users (owner));
+diesel::joinable!(user_connections -> users (account));
 
 diesel::allow_tables_to_appear_in_same_query!(
     api_keys,
